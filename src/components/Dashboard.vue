77eb3b0286c36196b74dd32b0dedcfe0cac90956
Aneeqa Khan <aneeqa.k25@gmail.com>
2019-12-20 12:54:18 +0500

    fetch data from firebase database

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="dashboard">
-    <h3>Dashbaord</h3>
+    <ul class="collection with-header">
+      <li class="collection-header">
+        <h4>Employees</h4>
+      </li>
+      <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
+        <div class="chip">{{employee.dept}}</div>
+        {{employee.employee_id}}:{{employee.name}}
+
+        <router-link v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}" class="secondary-content">
+          <i class="fa fa-eye"></i>
+        </router-link>
+      </li>
+    </ul>
+
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
         <i class="fa fa-plus"></i>
@@ -10,10 +23,27 @@
 </template>
 
 <script>
+import db from "./firebaseInit"
 export default {
   name: "dashboard",
   data() {
-    return {};
+    return {
+      employees: []
+    };
+  },
+  created() {
+    db.collection('employees').orderBy('dept').get().then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        const data = {
+          'id': doc.id,
+          'employee_id': doc.data().employee_id,
+          'name': doc.data().name,
+          'dept': doc.data().dept,
+          'position': doc.data().position
+        }
+        this.employees.push(data)
+      })
+    })
   }
 };
 </script>
