65887b222a5ef07e128290471c6bb2c307afaf1f
Aneeqa Khan <aneeqa.k25@gmail.com>
2019-12-20 11:13:30 +0500

    Create project and routes

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="dashboard">
+    <h3>Dashbaord</h3>
+    <div class="fixed-action-btn">
+      <router-link to="/new" class="btn-floating btn-large red">
+        <i class="fa fa-plus"></i>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "dashboard",
+  data() {
+    return {};
+  }
+};
+</script>
+
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBS31LgYF0U9UNo82_eSkwBZphN9FpQzgU",
+    authDomain: "vuefs-prod-8f671.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-8f671.firebaseio.com",
+    projectId: "vuefs-prod-8f671",
+    storageBucket: "vuefs-prod-8f671.appspot.com",
+    messagingSenderId: "227888026034",
+    appId: "1:227888026034:web:b7512ac0c0c5d41be414ab",
+    measurementId: "G-LDGVQDEYE5"
+}
